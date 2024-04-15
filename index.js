0e90192ae811d6773c543af5be518d8c011209dc
Polina <smivasilisa@gmail.com>
2024-04-15 17:38:04 +0200

    Express

--- index.js
+++ index.js
@@ -1,50 +1,17 @@
-const http = require("http");
-const fs = require("fs/promises");
+const express = require("express");
+
+const app = express();
 
 const PORT = 5000;
 
-const requestListener = async (request, response) => {
-  const { url, method } = request;
+app.get("/", (request, response) => {
+  response.send("Hello world?");
+});
 
-  if (method === "GET") {
-    if (url === "/index.html") {
-      try {
-        const data = await fs.readFile("./views/index.html", "utf-8");
-        response.statusCode = 200;
-        response.end(data);
-      } catch (error) {
-        response.statusCode = 404;
-        response.end();
-      }
-    } else if (url === "/style.css") {
-      try {
-        const data = await fs.readFile("./views/style.css", "utf-8");
-        response.statusCode = 200;
-        response.end(data);
-      } catch (error) {
-        response.statusCode = 404;
-        response.end();
-      }
-    } else {
-      response.statusCode = 404;
-      response.end();
-    }
-  } else if (method === "POST") {
-    if (url === "/user") {
-      let jsonString = "";
-      request.on("data", (chunk) => {
-        jsonString += chunk;
-      });
-      request.on("end", () => {
-        const user = JSON.parse(jsonString);
-        console.log(user);
-        response.statusCode = 200;
-        response.end();
-      });
-    }
-  }
-};
+app.get("/index.html", (require, response) => {
+  response.status(404).send("You f*ed up");
+});
 
-const server = http.createServer(requestListener);
-
-server.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
